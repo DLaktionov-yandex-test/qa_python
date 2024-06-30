be47290f493f5104618ebf66a3e7a09273a7ea80
Дмитрий <d.laktionov2022@mail.ru>
2024-06-30 17:27:18 +0300

    Написание авто-тестов

--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+import pytest
+import random
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -18,7 +20,114 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    @pytest.mark.parametrize('name_book', ['K', 'Ян', 'Название книги которое состоит из 40 сим'])
+    def test_add_new_book_boundary_value_positiv(self, name_book):
+        collector = BooksCollector()
+        collector.add_new_book(name_book)
+        assert name_book in collector.get_books_genre()
+
+    @pytest.mark.parametrize('name_book', ['', 'Название книги которое состоит больше 40 символов'])
+    def test_add_new_book_boundary_value_negative(self, name_book):
+        collector = BooksCollector()
+        collector.add_new_book(name_book)
+        assert name_book in collector.get_books_genre()
+
+    def test_set_book_genre_positive(self):
+        collector = BooksCollector()
+        name_book = 'Бойцовский клуб'
+        genre = 'Детективы'
+        collector.add_new_book(name_book)
+        collector.set_book_genre(name_book, genre)
+        assert collector.get_book_genre(name_book) == genre
+
+    def test_set_book_genre_no_value_in_genre_negative(self):
+        collector = BooksCollector()
+        name_book = 'Бойцовский клуб'
+        genre = 'Драма'
+        collector.add_new_book(name_book)
+        collector.set_book_genre(name_book, genre)
+        assert collector.get_book_genre(name_book) == genre
+
+    def test_get_books_genre_true_value_positive(self):
+        collector = BooksCollector()
+        genre_test_dict = {}
+        genre_test_dict['Бойцовский клуб'] = 'Детективы'
+        name_book = 'Бойцовский клуб'
+        genre = 'Детективы'
+        collector.add_new_book(name_book)
+        collector.set_book_genre(name_book, genre)
+        assert genre_test_dict == collector.get_books_genre()
+
+    def test_get_books_genre_instance_value(self):
+        collector = BooksCollector()
+        name_book = 'Бойцовский клуб'
+        genre = 'Детективы'
+        collector.add_new_book(name_book)
+        collector.set_book_genre(name_book, genre)
+        assert isinstance(collector.get_books_genre(), dict)
+        assert bool(collector.get_books_genre())
+
+    def test_get_books_with_specific_genre(self):
+        collector = BooksCollector()
+        books_list_sorting_for_genre = []
+        name_books = ['Бойцовский клуб', 'Первый мститель', 'В', 'Поворот не туда', 'Тайная жизнь домашних живаотных']
+
+        for i in name_books:
+            collector.add_new_book(i)
+            collector.set_book_genre(i, random.choice(collector.genre))
+
+        get_random_genre_in_dict = collector.get_book_genre(random.choice(name_books))
+
+        for key, i in collector.get_books_genre().items():
+            if i == get_random_genre_in_dict:
+                books_list_sorting_for_genre.append(key)
+
+        sorting_list_books_name = collector.get_books_with_specific_genre(get_random_genre_in_dict)
+
+        assert sorting_list_books_name == books_list_sorting_for_genre
+
+    def test_get_books_for_children(self):
+        collector = BooksCollector()
+        books_list_sorting_for_genre = []
+        name_books = ['Бойцовский клуб', 'Первый мститель', 'В', 'Поворот не туда', 'Тайная жизнь домашних живаотных']
+
+        for i in name_books:
+            collector.add_new_book(i)
+            collector.set_book_genre(i, random.choice(collector.genre))
+
+
+        for key, i in collector.get_books_genre().items():
+            if i not in collector.genre_age_rating:
+                books_list_sorting_for_genre.append(key)
+
+        sorting_list_books_name = collector.get_books_for_children()
+
+        assert sorting_list_books_name == books_list_sorting_for_genre
+
+    def test_add_book_in_favorites_positive(self):
+        collector = BooksCollector()
+        name_book = 'Бойцовский клуб'
+        collector.add_new_book(name_book)
+        collector.add_book_in_favorites(name_book)
+        assert name_book in collector.get_list_of_favorites_books()
+
+    def test_delete_book_from_favorites_poitive(self):
+        collector = BooksCollector()
+        name_book = 'Бойцовский клуб'
+        collector.add_new_book(name_book)
+        collector.add_book_in_favorites(name_book)
+        collector.delete_book_from_favorites(name_book)
+        assert name_book not in collector.get_list_of_favorites_books()
+
+    def test_get_list_of_favorites_books(self):
+        collector = BooksCollector()
+        name_book = 'Бойцовский клуб'
+        collector.add_new_book(name_book)
+        collector.add_book_in_favorites(name_book)
+        assert isinstance(collector.get_list_of_favorites_books(), list)
+        assert bool(collector.get_list_of_favorites_books())
